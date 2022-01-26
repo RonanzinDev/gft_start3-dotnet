8b948b4c4dd9789b1c3dbda40d950874e2fa7727
RonanzinDev <morellianogm@gmail.com>
2022-01-26 16:56:51 -0300

    Newteste

--- typereferencesandvalue/refword/Program.cs
+++ typereferencesandvalue/refword/Program.cs
@@ -2,6 +2,12 @@
 
 public class Program
 {
+    static void Demo1()
+    {
+        int a = 2;
+        Add20(ref a);
+    }
+    static void Add20(ref int a) => a += 20;
     static void Main()
     {
 
